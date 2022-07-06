c3caf9b397f3ece37d5885ffdf6315fc40c9ef4a
yuhwan-park <apkdkvkz@gmail.com>
2022-07-06 11:01:24 +0900

    Design: 메인컬러 설정

--- client/src/styled.d.ts
+++ client/src/styled.d.ts
@@ -2,6 +2,10 @@ import 'styled-components';
 
 declare module 'styled-components' {
   export interface DefaultTheme {
-    mainColor: string;
+    mainColor: {
+      normal: string;
+      lighter: string;
+      darker: string;
+    };
   }
 }
