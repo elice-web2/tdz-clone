91f8c4bd3b0526a6725fc13e869c11f280503bfe
user <yenje7@naver.com>
2022-07-12 12:25:10 +0900

    Refactor: 타입 인터페이스 분리

---
+++ server/src/customType/meal.type.ts
@@ -0,0 +1,30 @@
+export interface MealInfo {
+  code: string;
+  name: String;
+  kcal: Number;
+  carb: Number;
+  protein: Number;
+  fat: Number;
+  sugars: Number;
+  natruim: Number;
+  cholesterol: Number;
+  saturatedfatty: Number;
+  transfat: Number;
+  updated_date: Date;
+}
+
+export interface MealData {
+  _id: string;
+  code: string;
+  name: String;
+  kcal: Number;
+  carb: Number;
+  protein: Number;
+  fat: Number;
+  sugars: Number;
+  natruim: Number;
+  cholesterol: Number;
+  saturatedfatty: Number;
+  transfat: Number;
+  updated_date: Date;
+}
