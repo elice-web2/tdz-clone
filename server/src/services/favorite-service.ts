4948fd5d3ab34b631ceebb96c8198f541076a401
[SW2/서울/1반] 이주연 <its.about2weeks@gmail.com>
2022-07-13 14:27:50 +0900

    feat: 즐겨찾기 하나 삭제  서비스

--- server/src/services/favorite-service.ts
+++ server/src/services/favorite-service.ts
@@ -19,10 +19,26 @@ class FavoriteService {
   //사용자별 즐겨찾기 조회
   async getFavoritesByUser(userId: string): Promise<FavoriteData[]> {
     const favorites = await this.favoriteModel.findByUser(userId);
+
+    //반환값이 없을 경우 빈 객체 반환
     if (!favorites) {
       return {} as Promise<FavoriteData[]>;
     }
 
     return favorites;
+  }
+
+  async deleteOnFavorite(
+    favoriteId: string,
+  ): Promise<{ deletedCount?: number }> {
+    const favorite = await this.favoriteModel.findById(favoriteId);
+
+    if (!favorite) {
+      throw new Error(
+        '해당하는 즐겨찾기가 없습니다. 다시 한 번 확인해 주세요.',
+      );
+    }
+
+    return await this.favoriteModel.deleteOneFavorite(favoriteId);
   }
 }
