42a1ea664c73720f73c23474d585e9d6105ed7ca
yuhwan-park <apkdkvkz@gmail.com>
2022-07-14 14:21:12 +0900

    Feat: DateSlice 작성

--- client/src/slices/dateSlice.ts
+++ client/src/slices/dateSlice.ts
@@ -2,11 +2,11 @@ import { createSlice } from '@reduxjs/toolkit';
 import dayjs from 'dayjs';
 
 export interface DateState {
-  value: string;
+  date: string;
 }
 
 const initialState: DateState = {
-  value: dayjs().format('DD-MM-YYYY'),
+  date: dayjs().format('YYYY-MM-DD'),
 };
 
 // Slice 작성 예시
@@ -15,11 +15,13 @@ export const dateSlice = createSlice({
   name: 'date',
   initialState,
   reducers: {
-    chageDate: (state, { payload }) => {
-      state.value = payload;
+    update: (state, { payload }) => {
+      state.date = payload;
     },
   },
   // 비동기 로직이 들어갈 시 AsyncThunk 를 작성 후 extraReducer에 추가
 });
+
+export const { update } = dateSlice.actions;
 
 export default dateSlice.reducer;
