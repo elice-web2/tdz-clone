9a6b4e366bf7e885373a30c75cf317e5cda9d345
[SW2/서울/1반] 이주연 <its.about2weeks@gmail.com>
2022-07-15 00:11:59 +0900

    feat: 차트용 서비스 파일 작성 시작

---
+++ server/src/services/chart-service.ts
@@ -0,0 +1,18 @@
+import { mealhistoryModel, MealHistoryModel } from '../db';
+import {
+  MealHistoryInfo,
+  MealHistoryData,
+} from '../customType/mealhistory.type';
+import { calendarService, userService } from '../services';
+import { CalendarModel, calendarModel } from '../db';
+import { CalendarInfo, CalendarData } from '../customType/calendar.type';
+
+// 1. 일간 합 : 하루의 각합을 7일 전 정보까지 받아서 돌려주기
+
+// 2. 주간 합 : 7일로 총합하는 서비스 <- 이걸 4번 하면 월간
+
+// 3. 월간 합 : 월별로 합하는 서비스
+
+// 4. 주간 합을 받아서 4주 평균을 내기
+
+// 5. 월간 합을 받아서 3개월 평균을 내기
