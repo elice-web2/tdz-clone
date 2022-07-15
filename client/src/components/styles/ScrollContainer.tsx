aaecc6aad3ed3c42b52e79fbc3b7fdabd87dc00b
yuhwan-park <apkdkvkz@gmail.com>
2022-07-15 13:48:06 +0900

    Design: 스크롤바 스타일 추가

--- client/src/components/styles/ScrollContainer.tsx
+++ client/src/components/styles/ScrollContainer.tsx
@@ -4,4 +4,13 @@ export const ScrollContainer = styled.div<{ minusHeight: number }>`
   overflow-x: hidden;
   overflow-y: scroll;
   height: calc(100vh - ${({ minusHeight }) => minusHeight}px);
+
+  &::-webkit-scrollbar {
+    width: 4px;
+    margin-right: 2px;
+  }
+  &::-webkit-scrollbar-thumb {
+    border-radius: 2px;
+    background: #ccc;
+  }
 `;
