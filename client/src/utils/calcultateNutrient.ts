dcfd1409509686c717fe3d3a92ab5a491c06dd11
sehee <seeeh0128@gmail.com>
2022-07-15 15:23:43 +0900

    계산 NaN 오류 수정

--- client/src/utils/calcultateNutrient.ts
+++ client/src/utils/calcultateNutrient.ts
@@ -1,3 +1,6 @@
-export function calNutrient(nutrient: number, count: number) {
+export function calNutrient(nutrient: number, count: number | ''): number {
+  if (!count) {
+    return 0;
+  }
   return Number((nutrient * count).toFixed(2));
 }
