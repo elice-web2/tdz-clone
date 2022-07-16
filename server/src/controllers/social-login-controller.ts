8f01727c6686099c5b54aad759675c4245945d11
user <yenje7@naver.com>
2022-07-17 00:57:15 +0900

    refactor: 코드 수정

--- server/src/controllers/social-login-controller.ts
+++ server/src/controllers/social-login-controller.ts
@@ -61,8 +61,10 @@ class SocialLoginController {
       const response = await socialLoginService.kakaoLogoutService(
         accessTokenValue,
       );
-      res.clearCookie('token');
-      res.json({ success: true, data: '성공적으로 로그아웃 되었습니다.' });
+      res
+        .clearCookie('accessToken')
+        .clearCookie('token')
+        .json({ success: true, data: '성공적으로 로그아웃 되었습니다.' });
     } catch (err) {
       next(err);
     }
